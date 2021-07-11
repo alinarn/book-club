d682557ab49fbd6f5769b8b3cd8fb8dae3a369e3
Alina <alinarnautova@gmail.com>
2021-07-10 20:28:27 -0400

    add validations for currently reading book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,10 @@ class Book < ApplicationRecord
 
   validates :title, :description, :author, :publication_date, :image, :pages, presence: true
 
+  validates_each :status do |record, attr, value|
+    record.errors.add(attr, 'currently reading is already taken, add book to future read') if value == "currently_reading"
+  end
+
   mount_uploader :image, BookUploader
 
   enum status: {
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_212204) do
+ActiveRecord::Schema.define(version: 2021_07_10_225350) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "author"
     t.date "publication_date"
-    t.date "read_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "image"
