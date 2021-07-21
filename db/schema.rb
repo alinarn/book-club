798ab08b86d7641d3c7ba3b70a62c9d258d600d3
Alina <alinarnautova@gmail.com>
2021-07-20 21:05:07 -0400

    implement login with remembering

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_205404) do
+ActiveRecord::Schema.define(version: 2021_07_20_180741) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_07_19_205404) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
